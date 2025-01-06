268194fa78f9130f8027097cdcba527b689f6a5b
Silbye <silverxlxl9@gmail.com>
2025-01-07 02:16:43 +0300

    first commit

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -7,7 +7,7 @@ export default function Search() {
         <div className="search-box">
           <input type="text" placeholder="Что хотите посмотреть?" />
           <button>
-            <img src="img/search.svg" alt="search" />
+            <img src="/img/search.svg" alt="search" />
             <span>найти</span>
           </button>
         </div>
